887ac38ba0e1195f8b3852121a7d71dfb13750e0
Nikolay Lapshin <nlapshin1989@gmail.com>
2024-04-12 00:03:27 +0300

    Test

---
+++ src/components/UsersList.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1>UsersList</h1>
+
+  {{ users }}
+
+  <!-- <div v-if="isLoading">
+    ...loading
+  </div>
+  <div v-else> -->
+    <!-- <UserItem v-for="user in users" :key="user.id" :user="user"/> -->
+  <!-- </div> -->
+</template>
+
+<script>
+import UserItem from '@/components/UserItem.vue'
+
+// COMP -> STORE -> SERVICE -> 
+
+
+// 1
+export default {
+    name: 'UsersList',
+    components: {
+        UserItem
+    },
+    // свойства и методы
+    data: () => ({
+        // users: []
+
+        // // users: [],
+        isLoading: false
+    }),
+    // mounted, created, updated <---- lifeCycle events on component
+    // 2
+    mounted(){
+        // console.log(this)
+
+        // this.loadUsers()
+        // 2 -> 3
+        this.$store.dispatch('loadUsers')
+    },
+    computed: {
+        // 6
+        users(){
+            return this.$store.state.users
+        }
+    },
+    methods: {
+        // loadUsers(){
+        //    loadUsers().then(json => {
+        //         this.users = json
+        //         this.isLoading = false
+        //     })
+        // }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {computed, ref} from 'vue'
+import {useStore} from 'vuex'
+
+import TheWelcome from '@/components/TheWelcome.vue'
+import UsersList from '@/components/UsersList.vue'
+const username = ref('')
+
+const store = useStore()
+const countValue = computed(() => store.state.count)
+
+</script>
+
+<template>
+  <main>
+    <UsersList/>
+    <!-- count: {{countValue}}
+    
+    <input v-model="username" v-focus="7"/> -->
+  </main>
+</template>
